eba8ed2e9a6ed054bfcfc1fe323d5417f6634c0e
moritanian <pickit2express@gmail.com>
2025-07-23 02:20:50 +0900

    Fix English translation errors and typos
    - Fix spelling errors: 'happend' -> 'happened', 'configration' -> 'configuration'
    - Improve grammar and natural expression in error messages
    - Make translations consistent between en.ts and package.nls.json
    - Update VIEW_PDF text from 'View LaTeX PDF' to 'View PDF' for consistency
    - Fix various grammatical issues in LOGIN_FAILED, OFFLINE_ERROR, FILE_CONFLICT_DETECTED, etc.
    - Align meaning between Japanese and English translations

--- src/locale/en.ts
+++ src/locale/en.ts
@@ -12,31 +12,31 @@ export const enTranslation: Translation = {
   VIEW_LOG: 'View Log',
   RELOAD: 'Reload',
   OFFLINE: 'Offline',
-  VIEW_PDF: 'View LaTeX PDF',
+  VIEW_PDF: 'View PDF',
 
   DETAIL: 'Detail',
   NO_WORKSPACE_ERROR: 'No workspace found. Please open a workspace first.',
   LOGIN_SUCCEEDED: 'Login to Cloud LaTeX succeeded.',
   LOGIN_FAILED: `Login to Cloud LaTeX failed. 
-  Please generate a token from the Cloud LaTeX web site and enter the generated token from the \'Set Account\' button on the lower right.
-  (* The token will be deactivated after a certain period of time. In that case, you need to generate token again.)`,
-  OFFLINE_ERROR: `The network is offline or some trouble occur with the server.
+  Please generate a token from the Cloud LaTeX website and enter the generated token using the \'Set Account\' button in the lower right.
+  (* The token will be deactivated after a certain period of time. In that case, you need to generate a new token.)`,
+  OFFLINE_ERROR: `The network is offline or some trouble has occurred with the server.
   You can edit your files, but your changes will not be reflected on the server
-  until it is enable to communicate with the server.`,
+  until communication with the server is restored.`,
   COMPILATION_FAILED: 'Compilation failed.',
   FILE_SYNC_FAILED: 'File synchronization failed',
   FILE_SYNCHRONIZED: 'Project files have been synchronized!',
-  FILE_CHANGE_ERROR: 'File change error happened.',
-  UNEXPECTED_ERROR: 'Unexpected error happend.',
-  FILE_CONFLICT_DETECTED: 'Following files is both changed in the server and local:',
-  CONFIG_ENABLED_PLACE_ERROR: `Be sure to set ${CONFIG_NAMES.enabled} to true not at user\'s settings but at workspace settings.`,
-  CONFIG_PROJECTID_EMPTY_ERROR: 'ProjectId should be set in workspace configration file.',
+  FILE_CHANGE_ERROR: 'Unexpected file change was detected.',
+  UNEXPECTED_ERROR: 'Unexpected error happened.',
+  FILE_CONFLICT_DETECTED: 'The following files have been changed both on the server and locally:',
+  CONFIG_ENABLED_PLACE_ERROR: `Be sure to set ${CONFIG_NAMES.enabled} to true in workspace settings, not in user settings.`,
+  CONFIG_PROJECTID_EMPTY_ERROR: 'ProjectId should be set in the workspace configuration file.',
   HOW_TO_GENERATE_TOKEN: 'How to generate token',
   SETTING_README_URL: 'https://github.com/cloudlatex-team/cloudlatex-vscode-extension#setting',
   CHECK_DETAILS: 'Check details',
   NO_COMPILATION_TARGET: 'Compilation target is not set',
-  UNKNOWN_ERROR: 'Unknown error happens',
+  UNKNOWN_ERROR: 'An unknown error has occurred',
   PROJECT_UPDATED: 'Project setting has been updated',
-  FIRST_SYNC_NOTIFICATION: 'Do you want to sync the project \'$project_name\' ?',
+  FIRST_SYNC_NOTIFICATION: 'Do you want to sync the project \'$project_name\'?',
   NOT_EMPTY_DIRECTORY: 'The directory contains files. To prevent overwriting files, the directory to be synchronized must be empty.'
 };
